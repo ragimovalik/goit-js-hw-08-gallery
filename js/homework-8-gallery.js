08d0f6d1d2dd1810e849840a8c08ad864adc8c73
ragimovalik <ragimovalik@gmail.com>
2020-12-26 15:37:43 +0200

    Создана разметка массива по шаблону

--- js/homework-8-gallery.js
+++ js/homework-8-gallery.js
@@ -1,3 +1,14 @@
 import gallery from "./gallery-items.js";
+// console.log(gallery);
 
-console.log(gallery);
+const galleryListEl = document.querySelector(".js-gallery");
+
+const myString = (array) =>
+  array.reduce((string, { preview, original, description }) => {
+    return (
+      string +
+      `<li class="gallery__item"><a class="gallery__link" href=${original}><img class="gallery__image" src=${preview} data-source=${original} alt=${description}/></a></li>`
+    );
+  }, "");
+
+galleryListEl.insertAdjacentHTML("afterbegin", myString(gallery));
