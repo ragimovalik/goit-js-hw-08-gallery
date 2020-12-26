28c4f811253f7640e74da519045f44bc286609e6
ragimovalik <ragimovalik@gmail.com>
2020-12-26 19:56:45 +0200

    Закрытие модалки клавишей ESC

--- js/homework-8-gallery.js
+++ js/homework-8-gallery.js
@@ -17,9 +17,12 @@ const modalImageEl = modalEl.querySelector(".lightbox__image");
 const modalClosingBtn = modalEl.querySelector(
   'button[data-action="close-lightbox"]'
 );
+const modalOverlayEl = modalEl.querySelector(".lightbox__overlay");
 
 galleryListEl.addEventListener("click", galleryClickHandler);
 modalClosingBtn.addEventListener("click", modalCloseHandler);
+modalOverlayEl.addEventListener("click", modalCloseHandler);
+
 
 function galleryClickHandler(event) {
   event.preventDefault();
@@ -33,6 +36,8 @@ function galleryClickHandler(event) {
   modalOpenHandler();
 
   modalImageEl.setAttribute("src", eTarget.dataset.source);
+
+  window.addEventListener("keydown", modalClosinByEsc);
 }
 
 function modalOpenHandler() {
@@ -44,20 +49,19 @@ function modalCloseHandler() {
   modalImageEl.setAttribute("src", "");
 }
 
+function modalClosinByEsc(event) {
+  if (event.code === "Escape") {
+    modalCloseHandler();
+  }
+}
+
 /*Подзадачи (обязательные)
-
-Создание и рендер разметки по массиву данных и предоставленному шаблону.
-
-Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
-
-Открытие модального окна по клику на элементе галереи.
-
-Подмена значения атрибута src элемента img.lightbox__image.
-
-Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
-
-Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того, чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее. */
-
+-Создание и рендер разметки по массиву данных и предоставленному шаблону.
+-Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
+-Открытие модального окна по клику на элементе галереи.
+-Подмена значения атрибута src элемента img.lightbox__image.
+-Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
+-Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того, чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее. */
 // =============================================================
 /*Дополнительные задачи
 -Закрытие модального окна по клику на div.lightbox__overlay.
@@ -66,7 +70,4 @@ function modalCloseHandler() {
 клавишами "влево" и "вправо".
 */
 
-const modalOverlayEl = modalEl.querySelector('.lightbox__overlay');
-console.log(modalOverlayEl);
 
-modalOverlayEl.addEventListener('click', modalCloseHandler)
